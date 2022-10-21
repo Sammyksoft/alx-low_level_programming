607f27c5fa1e40e8b42e0b6d81f6d01f973441a1
sammyksoft <sammyksoft23@gmail.com>
2022-10-21 14:28:55 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,6 @@
 #include "main.h"
 
-
-
 /**
-
  * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
@@ -14,19 +11,12 @@ int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
-
 	{
-
 		return (1);
-
 	}
-
 	else
-
 	{
-
 		return (0);
-
 	}
 
 }
